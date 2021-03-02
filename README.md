0156bc30e3384db3d94c4fe5d891d6ba4738cc59
shine.xgh <shine.xgh@gmail.com>
2021-03-02 15:10:10 +0800

    add edk2

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # linux-labs
+
 linux labs
+
+## Requirements
+
+Ubuntu 20.04 x64
+
+kernel
+
+``` bash
+sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf # https://wiki.ubuntu.com/Kernel/BuildYourOwnKernel
+sudo apt install qemu qemu-system-arm gcc-aarch64-linux-gnu gdb-multiarch bc trace-cmd kernelshark bpfcc-tools cppcheck # running linux kernel
+```
+
+grub2
+
+``` bash
+sudo apt install libdevmapper-dev autopoint libsdl2-dev libpciaccess-dev libusb-dev libfreetype-dev unifont xorriso libfuse-dev fonts-dejavu zfsutils-linux
+sudo apt install ovmf ksmtuned
+```
---
+++ debug_kernel_boot.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cd $(dirname $) || exit $?
+
+LINUX_DIR=linux
+JOBCOUNT=${JOBCOUNT=$(nproc)}
+
+build_kernel() {
+    cd ${LINUX_DIR}
+    cp /boot/config-$(uname -r) .config
+    make oldconfig      # 根据提示安装`flex bison libelf-dev`等依赖包
+    make localmodconfig # 仅安装已有的module
+    make menuconfig
+    make -j${JOBCOUNT}
+    # got arch/x86_64/boot/bzImage
+}
+
+run_qemu() {
+    cd ${LINUX_DIR}
+    qemu-system-i386 arch/x86_64/boot/bzImage -nographic -s -S
+    # ctrl A + x to EXIT
+}
+
+run_gdb() {
+    gdb -ex "target remote :1234" \
+        -ex "set tdesc filename target.xml" \
+        -ex "set arch i8086" \
+        -ex "display/i \$cs*16+\$pc" \
+        -ex "break *0x7c00"
+}
+
+case $1 in
+
+build)
+    build_kernel
+    ;;
+
+run)
+    run_qemu
+    ;;
+
+gdb)
+    run_gdb
+    ;;
+
+*)
+    echo "usage: $0 build|run|gdb"
+    exit 1
+    ;;
+
+esac
